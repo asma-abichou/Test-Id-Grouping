179666dedd316b331931ca1b14234f7ff2123076
asma-ab <abichouasmaa@gmail.com>
2024-02-23 01:35:56 +0100

    edit member template -in process

--- src/Controller/MemberController.php
+++ src/Controller/MemberController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 use App\Entity\Member;
 use App\Repository\MemberRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,6 +13,12 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/dashboard')]
 class MemberController extends AbstractController
 {
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager
+    )
+    {
+    }
+
     #[Route('/member', name: 'app_member')]
     public function showMember(MemberRepository $memberRepository): Response
     {
@@ -20,20 +27,40 @@ class MemberController extends AbstractController
             "members" => $allMembers
         ]);
     }
-
-    #[Route('/member/edit', name: 'app_member_edit_form')]
-    public function editMemberList(): Response
+    #[Route('/member/{memberId}/edit', name: 'app_member_form')]
+    public function showEditMember($memberId, MemberRepository $memberRepository): Response
     {
+        $memberToEdit = $memberRepository->find($memberId);
         return $this->render('member/edit-member.html.twig', [
-            'controller_name' => 'MemberController',
+            'memberToEdit' => $memberToEdit,
         ]);
     }
     #[Route('/member/{memberId}/edit', name: 'app_member_edit', methods: 'POST')]
-    public function editMemberProcess(Request $request, MemberRepository $memberRepository): Response
+    public function editMemberProcess(Request $request, $memberId, MemberRepository $memberRepository): Response
     {
-        return $this->render('member/edit-member.html.twig', [
-            'controller_name' => 'MemberController',
-        ]);
-    }
+        //dd('test');
+        $data = $request->request->all();
+        $name = $data["name"];
+        $email = $data["email"];
+        $city = $data["city"];
 
+        if(($name === "") || ($email === "")|| ($city === ""))
+        {
+            $this->addFlash('editMemberWarning', 'Please fill all the fields!');
+            return $this->redirectToRoute('app_member_form', [
+                "memberId" => $memberId
+            ]);
+        }
+        $memberToEdit = $memberRepository->find($memberId);
+        $memberToEdit->setName($name);
+        $memberToEdit->setEmail($email);
+        $memberToEdit->setCity($city);
+        //dump($memberToEdit);
+        $this->entityManager->persist($memberToEdit);
+        $this->entityManager->flush();
+        $this->addFlash('editSuccess', 'Student Edited Successfully!');
+
+        return $this->redirectToRoute('app_member');
+
+    }
 }
